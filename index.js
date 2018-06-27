d0976dd66d0c403c919b56eddea42e6f100d367e
Izychiel <malo.grall@gmail.com>
2018-06-28 00:31:49 +0200

    URL reading and choosing text

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function (req, res) {
+	const page = url.parse(req.url).pathname;
+
 	res.writeHead('200', {"Content-Type": "text/plain"});
-	res.write('Inconnu');
+
+	// REDIRECT TO MANY PAGES
+	if(page === '/' || page === '/index'){
+		res.write('acceuil');
+	}else if (page === '/page'){
+		res.write('page');
+	}else{	// 404 PAGE
+		res.writeHead('404');
+		res.write('Inconnu');
+	}
 	res.end()
 });
 server.listen(8080);
