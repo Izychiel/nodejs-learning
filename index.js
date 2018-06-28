0946a2d6ee8ab0ed293acb0300eefe0763875378
Izychiel <malo.grall@gmail.com>
2018-06-28 20:02:17 +0200

    Express 404 page + new function form

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {	// HOME
 	res.setHeader('Content-Type', 'text/plain');
 	res.send('Accueil')
+});
+
+// 404 PAGE
+app.use((req, res, next) => {	// A CERTAIN FORM OF FUNCTION
+	res.setHeader('Content-Type', 'text/plain');
+	res.status(404).send('Unfound page')
 });
 
 app.listen(8080);
