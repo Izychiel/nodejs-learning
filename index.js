6709521ef0e34ff298b7957eab4c20ebc7a1d651
Izychiel <malo.grall@gmail.com>
2018-06-29 00:49:37 +0200

    Used express with template -> EJS

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const app = express();
 app.get('/', function (req, res) {	// HOME
 	res.setHeader('Content-Type', 'text/plain');
 	res.send('Accueil')
-});
+})
+
+.get('/user/:pseudo',(req, res) => {
+	res.setHeader('Content-Type', 'text/plain');
+	res.send(req.params.pseudo);	// A PARAMETER FROM URL
+})
+
+.get('/ejs/:val', function (req, res) {
+	res.setHeader('Content-Type', 'text/html');
+
+	const allVal = req.params.val.split('&');
+	res.render('ejstest.ejs', {val: allVal});
+})
 
 // 404 PAGE
-app.use((req, res, next) => {	// A CERTAIN FORM OF FUNCTION
+.use((req, res, next) => {	// A CERTAIN FORM OF FUNCTION
 	res.setHeader('Content-Type', 'text/plain');
-	res.status(404).send('Unfound page')
+	res.status(404).send('Not found page')
 });
 
 app.listen(8080);
