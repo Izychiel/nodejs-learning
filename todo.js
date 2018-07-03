85b2f3e5d6862ff4720d162b3d4bb2b6e200c3fa
Izychiel <malo.grall@gmail.com>
2018-07-03 16:16:12 +0200

    Showing todos and can add

--- todo.js
+++ todo.js
@@ -1,23 +1,35 @@
 const express = require('express');
+const session = require('cookie-session');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/todo',function (req, res) {
+app.use(session({secret:'todosecret'}))
+	.use(bodyParser.urlencoded({extended: false}))
+
+.get('/todo', (req, res) => {	// CHARGE THE TODO LIST
 	res.setHeader('Content-Type','text/html');
-	res.render('todo-list.ejs')
+	req.session[0] = 'test';
+	res.render('todo-list.ejs', {session: req.session});
+	res.end();
 })
 
-.post('/todo/add',function (req, res) {
-
+.post('/todo/add',function (req, res) {	// ADDING A TODO
+	res.setHeader('Content-Type', 'text/plain');
+	req.session[req.session.length] = req.body.value;
+	res.end();
 })
 
-.post('/todo/del/:id',function (req, res) {
-
+.post('/todo/del/:id',function (req, res) {	// DELETING A TODO
+	res.setHeader('Content-Type', 'text/plain');
+	res.end();
 })
 
-.use(function (req, res, next) {
+.use((req, res, next) => {	// 404 PAGE
 	res.setHeader('Content-Type','text/html');
 	res.status(404);
-	res.render('todo-404.ejs', {path: req.originalUrl})
+	res.render('todo-404.ejs', {path: req.originalUrl, session: req.session})
+	res.end();
 });
 
 app.listen(8080);
