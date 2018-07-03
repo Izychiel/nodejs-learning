788e66df2dd5c544e00ad447a5b010f55dc18d79
Izychiel <malo.grall@gmail.com>
2018-07-03 18:56:05 +0200

    Added delete

--- todo.js
+++ todo.js
@@ -9,26 +9,37 @@ app.use(session({secret:'todosecret'}))
 
 .get('/todo', (req, res) => {	// CHARGE THE TODO LIST
 	res.setHeader('Content-Type','text/html');
-	req.session[0] = 'test';
+
 	res.render('todo-list.ejs', {session: req.session});
 	res.end();
 })
 
 .post('/todo/add',function (req, res) {	// ADDING A TODO
 	res.setHeader('Content-Type', 'text/plain');
+
 	req.session[req.session.length] = req.body.value;
 	res.end();
 })
 
-.post('/todo/del/:id',function (req, res) {	// DELETING A TODO
+.get('/todo/del/:id',function (req, res) {	// DELETING A TODO
 	res.setHeader('Content-Type', 'text/plain');
+	const paramId = parseInt(req.params.id);
+
+	if(paramId !== req.session.length-1){
+		for(let i=paramId; i<req.session.length; i++){	// SHIFT ALL VALUES IF IT ISN'T THE LAST ONE
+			req.session[i] = req.session[i+1];
+		}
+		delete req.session[req.session.length-1];
+	}else{
+		delete req.session[paramId];
+	}
 	res.end();
 })
 
 .use((req, res, next) => {	// 404 PAGE
 	res.setHeader('Content-Type','text/html');
 	res.status(404);
-	res.render('todo-404.ejs', {path: req.originalUrl, session: req.session})
+	res.render('todo-404.ejs', {path: req.originalUrl, session: req.session});
 	res.end();
 });
 
