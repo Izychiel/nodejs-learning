c5279feb554a5db4f8cfb3e6fe8e35cab44d4402
Izychiel <malo.grall@gmail.com>
2018-07-03 19:18:54 +0200

    Using splice instead of 'delete' and using a table in sessions

--- todo.js
+++ todo.js
@@ -6,18 +6,24 @@ const app = express();
 
 app.use(session({secret:'todosecret'}))
 	.use(bodyParser.urlencoded({extended: false}))
+	.use(function (req, res, next) {
+		if(typeof(req.session.todolist) === 'undefined'){
+			req.session.todolist =[];
+		}
+		next();
+	})
 
 .get('/todo', (req, res) => {	// CHARGE THE TODO LIST
 	res.setHeader('Content-Type','text/html');
 
-	res.render('todo-list.ejs', {session: req.session});
+	res.render('todo-list.ejs', {todolist: req.session.todolist});
 	res.end();
 })
 
 .post('/todo/add',function (req, res) {	// ADDING A TODO
 	res.setHeader('Content-Type', 'text/plain');
 
-	req.session[req.session.length] = req.body.value;
+	req.session.todolist[req.session.todolist.length] = req.body.value;
 	res.end();
 })
 
@@ -25,14 +31,7 @@ app.use(session({secret:'todosecret'}))
 	res.setHeader('Content-Type', 'text/plain');
 	const paramId = parseInt(req.params.id);
 
-	if(paramId !== req.session.length-1){
-		for(let i=paramId; i<req.session.length; i++){	// SHIFT ALL VALUES IF IT ISN'T THE LAST ONE
-			req.session[i] = req.session[i+1];
-		}
-		delete req.session[req.session.length-1];
-	}else{
-		delete req.session[paramId];
-	}
+	req.session.todolist.splice(paramId, 1);	// DELETING IT
 	res.end();
 })
 
