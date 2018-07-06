fa2c99d1e2427730d0e9c69cb40f9d252ba735e2
Izychiel <malo.grall@gmail.com>
2018-07-06 18:00:18 +0200

    Chat started socket.io, can send and receive msg (miss join and msg charge on login)

--- socket.js
+++ socket.js
@@ -8,13 +8,27 @@ server.listen(8080);
 
 app.use(express.static('public'))
 
-.get('/', function (req, res) {
+.get('/', function (req, res) {	// TRY WITH SOCKET
 	res.setHeader('Content-Type', 'text/html');
 	res.render('socket.ejs');
 	res.end();
 });
-
-io.sockets.on('connection',function (socket) {
+io.sockets.on('connection',function (socket) {	// CONSOLE MSG ON CONNECT
 	console.log('New viewer');
 	socket.emit('message', 'Hello');
+});
+
+
+app.get('/chat',function (req, res) {
+	res.setHeader('Content-Type', 'text/html');
+	res.render('chat.ejs');
+	res.end();
+});
+io.sockets.on('connection',function (socket) {
+	socket.on('login',function (pseudo) {	// SEND THE NEW USER
+		socket.broadcast.emit('login', pseudo);
+	});
+	socket.on('chat',function (message) {	// SENDING MSG TO OTHERS
+		socket.broadcast.emit('chat', {'pseudo': message['pseudo'], 'message': message['message']});
+	})
 });
